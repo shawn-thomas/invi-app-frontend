bdb5e109acf52b593c412569725f4d8159c7c74b
Shawn <sljt@proton.me>
2023-11-19 16:11:45 -0500

    update route list with logged in views

--- src/RoutesList.js
+++ src/RoutesList.js
@@ -1,7 +1,8 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Homepage from './Homepage/Homepage';
-import LoginForm from './LoginSignup/LoginForm'
-import SignupForm from './LoginSignup/SignupForm'
+import Homepage from './components/LoggedOut/Homepage/Homepage';
+import LoginForm from './components/LoggedOut/LoginSignup/LoginForm';
+import SignupForm from './components/LoggedOut/LoginSignup/SignupForm';
+import Dashboard from './components/LoggedIn/Dashboard';
 
 /** Define routes.
  *
@@ -16,15 +17,23 @@ import SignupForm from './LoginSignup/SignupForm'
  */
 
 function RoutesList({ signUp, login, auth }) {
-  if (!auth){
+  if (!auth) {
     return (
       <Routes>
-        <Route path="/login" element={ <LoginForm login={login}/>}/>
-        <Route path="/signup" element={ <SignupForm signUp={signUp}/>}/>
-        <Route path="/" element={ <Homepage />}/>
-        <Route path="*" element={ <Navigate to="/" />}/>
+        <Route path="/login" element={<LoginForm login={login} />} />
+        <Route path="/signup" element={<SignupForm signUp={signUp} />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    )
+    );
+  }
+
+  else {
+    return (
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    );
   }
 
 }
