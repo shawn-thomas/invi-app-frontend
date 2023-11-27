b45dec22d09ec02c1c00fdf8de76cfb76dec8a36
Shawn Thomas <sljt@proton.me>
2023-11-27 14:26:30 -0500

    WIP: inventory tables

---
+++ src/components/LoggedIn/modals/AddProduct.js
@@ -0,0 +1,177 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import InviApi from '../../../api';
+import Alert from '../../../common/Alert';
+
+
+function AddProduct({ isOpen, onClose, onFetchCustomer }) {
+  const [newProductData, setNewProductData] = useState({
+    sku: '',
+    productName: '',
+    description: '',
+    price: '',
+    quantityAvailable: '',
+  });
+
+  console.log(newProductData)
+
+  const [validationErrors, setValidationErrors] = useState({});
+  const [formErrors, setFormErrors] = useState([]);
+
+  /** Handles input changes and updates the new customer data. */
+  function handleInputChange(evt) {
+    const { name, value } = evt.target;
+    setNewProductData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+    setValidationErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: '',
+    }));
+  }
+
+  /** Validates the form and displays errors. */
+  function validateForm() {
+    const errors = {};
+
+    if (!newProductData.sku) {
+      errors.sku = 'SKU is required';
+    }
+    if (!newProductData.productName) {
+      errors.name = 'Product name is required';
+    }
+    if (!newProductData.price) {
+      errors.price = 'Price is required';
+    }
+    if (!newProductData.quantityAvailable) {
+      errors.quantity = 'Quantity is required';
+    }
+
+    setValidationErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  }
+
+  /** Handles the addition of a new product. */
+  async function handleAddProduct() {
+    try {
+      if (!validateForm()) {
+        return;
+      }
+
+      await InviApi.createProduct(newProductData);
+
+      setNewProductData({
+        sku: '',
+        productName: '',
+        description: '',
+        price: '',
+        quantityAvailable: '',
+      });
+
+      setFormErrors([]);
+      onFetchCustomer();
+      onClose();
+
+    } catch (error) {
+      console.error('Error adding customer:', error.message);
+      setFormErrors(error);
+    }
+  };
+
+
+  /** Closes the dialog and resets the form. */
+  function handleClose() {
+    setNewProductData({
+      sku: '',
+      productName: '',
+      description: '',
+      price: '',
+      quantityAvailable: '',
+    });
+
+    setFormErrors([]);
+    onClose();
+  };
+
+  return (
+    <Dialog open={isOpen} onClose={handleClose}>
+      <DialogTitle>Create New Product</DialogTitle>
+      <DialogContent>
+        {formErrors.length > 0 && (
+          <Alert messages={formErrors} />
+        )}
+
+        <TextField
+          label="SKU"
+          name="sku"
+          value={newProductData.sku}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          error={Boolean(validationErrors.sku)}
+          helperText={validationErrors.sku}
+          required
+        />
+        <TextField
+          label="Name"
+          name="productName"
+          value={newProductData.productName}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          error={Boolean(validationErrors.productName)}
+          helperText={validationErrors.productName}
+          required
+        />
+        <TextField
+          label="Description"
+          name="description"
+          value={newProductData.description}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Price"
+          name="price"
+          value={newProductData.price}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          error={Boolean(validationErrors.price)}
+          helperText={validationErrors.price}
+          required
+        />
+        <TextField
+          label="Quantity"
+          name="quantityAvailable"
+          value={newProductData.quantityAvailable}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+          error={Boolean(validationErrors.quantityAvailable)}
+          helperText={validationErrors.quantityAvailable}
+          required
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleAddProduct} variant="outlined" color="primary">
+          Add Product
+        </Button>
+        <Button onClick={handleClose} color="error">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default AddProduct;
