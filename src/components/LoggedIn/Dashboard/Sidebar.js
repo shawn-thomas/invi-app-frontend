02c5f8024bff9d29fdd23e0d5b4394ff03906b6c
Shawn <sljt@proton.me>
2023-12-03 01:17:06 -0500

    removed text decoration from sidebar links

--- src/components/LoggedIn/Dashboard/Sidebar.js
+++ src/components/LoggedIn/Dashboard/Sidebar.js
@@ -1,5 +1,5 @@
-import '../styles/Sidebar.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleSharpIcon from '@mui/icons-material/PeopleSharp';
 import QrCodeSharpIcon from '@mui/icons-material/QrCodeSharp';
@@ -8,7 +8,8 @@ import InventorySharpIcon from '@mui/icons-material/InventorySharp';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import LogoutSharpIcon from '@mui/icons-material/LogoutSharp';
-import { useNavigate, Link } from "react-router-dom";
+import '../styles/Sidebar.css';
+
 
 
 function Sidebar({ logout, onSidebarItemClick }) {
@@ -30,24 +31,24 @@ function Sidebar({ logout, onSidebarItemClick }) {
             <DashboardIcon className="sidebar-li-icon" />
             {isExpanded && <span>Dashboard</span>}
           </li>
-          <Link to="/dashboard/customers">
-          <li>
-            <PeopleSharpIcon className="sidebar-li-icon" />
-            {isExpanded && <span>Customers</span>}
-          </li>
+          <Link className="sidebar-link" to="/dashboard/customers">
+            <li>
+              <PeopleSharpIcon className="sidebar-li-icon" />
+              {isExpanded && <span>Customers</span>}
+            </li>
           </Link>
-          <Link to="/dashboard/inventory">
-          <li>
-            <QrCodeSharpIcon className="sidebar-li-icon" />
-            {isExpanded && <span>Inventory</span>}
-          </li>
+          <Link className="sidebar-link" to="/dashboard/inventory">
+            <li>
+              <QrCodeSharpIcon className="sidebar-li-icon" />
+              {isExpanded && <span>Inventory</span>}
+            </li>
           </Link>
-          <li>
-            <Link to="/dashboard/invoice">
-            <RequestPageSharpIcon className="sidebar-li-icon" />
-            {isExpanded && <span>Invoices</span>}
-            </Link>
-          </li>
+          <Link className="sidebar-link" to="/dashboard/invoice">
+            <li>
+              <RequestPageSharpIcon className="sidebar-li-icon" />
+              {isExpanded && <span>Invoices</span>}
+            </li>
+          </Link>
           <li>
             <InventorySharpIcon className="sidebar-li-icon" />
             {isExpanded && <span>Audit</span>}
