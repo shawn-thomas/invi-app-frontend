290f2e58375e73888b32e0d18081249cab3b7df1
Shawn Thomas <sljt@proton.me>
2023-12-19 21:02:01 -0500

    refactor ProductList with material-react-tables

--- src/components/LoggedIn/Lists/ProductList.js
+++ src/components/LoggedIn/Lists/ProductList.js
@@ -1,178 +1,379 @@
 import React, { useState } from 'react';
 import {
+  Box,
   Button,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  TextField,
-} from '@mui/material'
-import AddIcon from '@mui/icons-material/Add';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+  Breadcrumbs,
+  Link,
+  Typography,
+  Tooltip,
+  IconButton
+} from '@mui/material';
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+  MRT_GlobalFilterTextField,
+  MRT_ToggleFiltersButton,
+} from 'material-react-table';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import * as XLSX from 'xlsx';
-import InviApi from '../../../api';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { mkConfig, generateCsv, download } from 'export-to-csv';
+import SnackbarAlert from '../../../common/SnackbarAlert';
 import AddProduct from '../modals/AddProduct';
-import '../styles/GenericList.css';
+import DeleteProduct from '../modals/DeleteProduct';
+import EditProduct from '../modals/EditProduct';
+import InviApi from '../../../api';
 
 
-function ProductList({ listData, onFetchProducts }) {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [isAddModalOpen, setAddModalOpen] = useState(false);
+function ProductList({ listData, onFetchProducts, onFetchAudit }) {
+  const [modalState, setModalState] = useState({
+    isAddModalOpen: false,
+    isDeleteModalOpen: false,
+    isEditModalOpen: false,
+  });
 
-  // Filter list based on the search query
-  const filteredRows = (Array.isArray(listData) && listData.length > 0)
-    ? listData.filter((row) => {
-      // Check if any value in the curr row contains the search query
-      const rowContainsQuery = Object.values(row).some((value) => {
+  const [productState, setProductState] = useState({
+    selectedSku: null,
+    selectedProductData: null,
+    updatedProductData: null,
+    formErrors: [],
+  });
 
-        if (typeof (value) === "string") {
-          return value.toLowerCase().includes(searchQuery.toLowerCase());
-        }
-        return false;
-      });
+  const [snackbarState, setSnackbarState] = useState({
+    open: false,
+    message: '',
+    severity: 'success',
+  });
 
-      return rowContainsQuery;
-    })
-    : [];
+  function handleModalToggle(modalName, isOpen) {
+    setModalState((prevState) => ({
+      ...prevState,
+      [modalName]: isOpen,
+    }));
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        accessorKey: 'sku',
+        header: 'SKU',
+      },
+      {
+        accessorKey: 'productName',
+        header: 'Product Name',
+      },
+      {
+        accessorKey: 'description',
+        header: 'Description',
+      },
+      {
+        accessorKey: 'price',
+        header: 'Price',
+        filterFn: 'between',
+        Cell: ({ cell }) => `$${cell.getValue()}`
+      },
+      {
+        accessorKey: 'quantityAvailable',
+        header: 'Quantity In Stock',
+        filterFn: 'between'
+      },
+      {
+        accessorKey: 'actions',
+        header: 'Action(s)',
+        enableColumnFilter: false,
+        enableSorting: false,
+        enableColumnActions: false,
+        Cell: ({ row }) => (
+          <>
+            <Tooltip title="Edit">
+              <IconButton
+                onClick={() => handleEditModalOpen(row.original)}>
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Delete">
+              <IconButton
+                color="error"
+                onClick={() => handleDeleteModalOpen(row.original.sku)}>
+                <DeleteIcon />
+              </IconButton>
+            </Tooltip>
+          </>
+        ),
+      }
+    ],
+    [],
+  );
+
+  /** Excel export ---------------------------------------------------------------*/
+
+  const csvConfig = mkConfig({
+    fieldSeparator: ',',
+    decimalSeparator: '.',
+    useKeysAsHeaders: true,
+  });
 
 
-  const formattedRows = filteredRows.map((row) => ({
-    sku: row.sku,
-    name: row.productName,
-    description: row.description,
-    price: row.price,
-    stock: row.quantityAvailable
-  }));
+  function handleExportData() {
+    const csv = generateCsv(csvConfig)(listData);
+    download(csvConfig)(csv);
+  };
 
-  /** Pagination ------------------------------------------------------------ */
+  function handleExportRows(rows) {
+    const rowData = rows.map((row) => row.original);
+    const csv = generateCsv(csvConfig)(rowData);
+    download(csvConfig)(csv);
+  };
 
-  /** Handle change for current page. */
-  function handleChangePage(evt, newPage) {
-    setPage(newPage);
-  }
-
-  /** Handle change of number of rows per page. */
-  function handleChangeRowsPerPage(evt) {
-    setRowsPerPage(parseInt(evt.target.value, 10));
-    setPage(0);
-  }
-
-  /** Add Customer --------------------------------------------------------------*/
+  /** Add Product --------------------------------------------------------------*/
 
   function handleAddModalOpen() {
-    setAddModalOpen(true);
-  };
+    handleModalToggle('isAddModalOpen', true);
+  }
 
   function handleAddModalClose() {
-    setAddModalOpen(false);
+    handleModalToggle('isAddModalOpen', false);
+  }
+
+  /** Product Delete -----------------------------------------------------------*/
+
+  function handleDeleteModalOpen(sku) {
+    setProductState((prevState) => ({
+      ...prevState,
+      selectedSku: sku,
+    }));
+    handleModalToggle('isDeleteModalOpen', true);
+  }
+
+  async function handleDeleteProduct() {
+    setSnackbarState({
+      open: true,
+      message: `${productState.selectedSku} deleted successfully`,
+      severity: 'warning',
+    });
+
+    await InviApi.removeProduct(productState.selectedSku);
+    onFetchProducts();
+    handleModalToggle('isDeleteModalOpen', false);
+  }
+
+  /** Product Edit ----------------------------------------------------------*/
+
+  function handleEditModalOpen(productData) {
+    setProductState((prevState) => ({
+      ...prevState,
+      selectedProductData: productData,
+      formErrors: [],
+    }));
+    handleModalToggle('isEditModalOpen', true);
+  }
+
+  async function handleUpdateProduct(updatedData) {
+    try {
+      const { productName, description, price, quantityAvailable, sku } = updatedData;
+      const numericPrice = parseFloat(price);
+      const numericQty = parseFloat(quantityAvailable);
+
+      const updatedProduct = {
+        productName,
+        description,
+        price: numericPrice,
+        quantityAvailable: numericQty,
+      }
+
+      await InviApi.updateProduct(sku, updatedProduct);
+
+      setProductState((prevState) => ({
+        ...prevState,
+        selectedProductData: updatedProduct,
+      }));
+
+      // Check if the new quantity is greater than the previous quantity
+      if (updatedProduct.quantityAvailable > productState?.selectedProductData.quantityAvailable){
+        onFetchAudit()
+      }
+
+      onFetchProducts();
+      handleModalToggle('isEditModalOpen', false);
+      setSnackbarState({
+        open: true,
+        message: `${productName} updated successfully!`,
+        severity: 'success',
+      });
+
+    } catch (error) {
+
+      setProductState((prevState) => ({
+        ...prevState,
+        formErrors: error,
+      }));
+    }
+  }
+
+  /** Snackbar Alert -----------------------------------------------------------*/
+
+  function handleSnackbarClose() {
+    setSnackbarState({ ...snackbarState, open: false });
   };
 
+  const table = useMaterialReactTable({
+    columns,
+    data: listData,
+    // enableRowNumbers: true,
+    enableRowSelection: true,
+    paginationDisplayMode: 'pages',
+    positionToolbarAlertBanner: 'bottom',
+    manualSorting: false,
+    initialState: { showColumnFilters: true, showGlobalFilter: true },
+    muiSearchTextFieldProps: {
+      size: 'small',
+      variant: 'outlined',
+    },
+    muiPaginationProps: {
+      color: 'primary',
+      rowsPerPageOptions: [5, 10, 20],
+      shape: 'rounded',
+      variant: 'outlined',
+    },
+    renderTopToolbar: ({ table }) => (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          gap: '16px',
+          padding: '1em',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          backgroundColor: '#f5f5f5',
+        }}
+      >
+        <Box sx={{
+          display: 'flex',
+          gap: '0.5rem',
+          alignItems: 'center'
+        }}>
+          <MRT_GlobalFilterTextField table={table} />
+          <MRT_ToggleFiltersButton table={table} />
+        </Box>
+        <Box sx={{
+          display: 'flex',
+          gap: '0.5rem',
+          alignItems: 'center'
+        }}>
+          <Tooltip title="Create New Product">
+            <Button
+              variant="contained"
+              color="primary"
+              size='small'
+              startIcon={<AddIcon />}
+              onClick={handleAddModalOpen}
+            >
+              Create
+            </Button>
+          </Tooltip>
+        </Box>
+      </Box>
+    ),
+    renderRowActions: ({ row, table }) => (
+      <Box sx={{ display: 'flex', gap: '1rem' }}>
+        <Tooltip title="Edit">
+          <IconButton onClick={() => table.setEditingRow(row)}>
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton color="error" onClick={() => handleDeleteModalOpen(row)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    ),
+  });
 
-  /** Excel export */
-  function handleExportToExcel() {
-    const ws = XLSX.utils.json_to_sheet(formattedRows);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Inventory');
-    XLSX.writeFile(wb, 'inventory.xlsx');
-  };
-
-  console.log("products", listData)
   return (
     <>
-      <div className="dashboard-header">
-        <div className="dashboard-title"><h2>Inventory</h2></div>
-        <div className="dashboard-create-btn">
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<AddIcon />}
-            sx={{ marginBottom: 2, height: 36 }}
-            onClick={handleAddModalOpen}
-          >
-            Create
-          </Button>
-          <Button
-            variant="outlined"
-            startIcon={<FileDownloadIcon />}
-            sx={{ marginBottom: 2, marginLeft: 2 }}
-            onClick={handleExportToExcel}>
-            Export
-          </Button>
-        </div>
-      </div>
-      <TextField
-        label="Search"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="db-table-search"
-        sx={{ marginBottom: 2 }}
+      <SnackbarAlert
+        open={snackbarState.open}
+        message={snackbarState.message}
+        severity={snackbarState.severity}
+        onClose={handleSnackbarClose}
       />
-      <TableContainer component={Paper} className="dashboard-table">
-        <Table sx={{ minWidth: 650, border: '1px solid #e0e0e0' }} aria-label="simple table">
-          <TableHead >
-            <TableRow sx={{ backgroundColor: '#f5f5f5' }}>
-              <TableCell className="db-table-head">SKU</TableCell>
-              <TableCell className="db-table-head">Name</TableCell>
-              <TableCell className="db-table-head">Description</TableCell>
-              <TableCell className="db-table-head">Price</TableCell>
-              <TableCell className="db-table-head">Quantity</TableCell>
-              <TableCell className="db-table-head">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? formattedRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : formattedRows
-            ).map((row) => (
-              <TableRow key={row.id}>
-                <TableCell className="db-table-cell">{row.sku}</TableCell>
-                <TableCell className="db-table-cell">{row.name}</TableCell>
-                <TableCell className="db-table-cell">{row.description}</TableCell>
-                <TableCell className="db-table-cell">{row.price}</TableCell>
-                <TableCell className="db-table-cell">{row.stock}</TableCell>
-                <TableCell className="db-table-cell">
-                  <EditOutlinedIcon
-                    style={{
-                      cursor: 'pointer',
-                      marginRight: '5px'
-                    }}
-                  />
-                  <DeleteOutlinedIcon
-                    style={{ cursor: 'pointer' }}
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 50]}
-          component="div"
-          count={formattedRows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </TableContainer>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          gap: '16px',
+          padding: '8px',
+          paddingLeft: '0px',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+        }}
+      >
+        <Box>
+          <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+            <Link
+              color="inherit"
+              href="/dashboard"
+              sx={{
+                fontSize: '0.9rem',
+                textDecoration: 'none'
+              }}>
+              Dashboard
+            </Link>
+            <Typography
+              color="text.primary"
+              sx={{ fontSize: '0.9rem' }}>
+              Inventory
+            </Typography>
+          </Breadcrumbs>
+        </Box>
+        <Box>
+          <Button
+            //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+            onClick={handleExportData}
+            startIcon={<FileDownloadIcon />}
+            size='small'
+          >
+            Export All
+          </Button>
+          <Button
+            disabled={
+              !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
+            }
+            //only export selected rows
+            onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+            startIcon={<FileDownloadIcon />}
+            size='small'
+          >
+            Export Selected
+          </Button>
+        </Box>
+      </Box>
+      <h2>Inventory</h2>
+
+      <MaterialReactTable table={table}
+      />
       <AddProduct
-        isOpen={isAddModalOpen}
+        isOpen={modalState.isAddModalOpen}
         onClose={handleAddModalClose}
         onFetchProduct={onFetchProducts}
       />
-      <div className="dashboard-export-btn">
-      </div>
+      <DeleteProduct
+        isOpen={modalState.isDeleteModalOpen}
+        onClose={() => handleModalToggle('isDeleteModalOpen', false)}
+        onConfirm={handleDeleteProduct}
+      />
+      <EditProduct
+        isOpen={modalState.isEditModalOpen}
+        onClose={() => handleModalToggle('isEditModalOpen', false)}
+        onUpdate={handleUpdateProduct}
+        initialData={productState.selectedProductData}
+        formErrors={productState.formErrors}
+      />
     </>
   );
-
-}
+};
 
 export default ProductList;
