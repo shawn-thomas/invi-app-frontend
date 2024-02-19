91d6f9df699df2e17616fa6a22510fac43152b21
Shawn Thomas <sljt@proton.me>
2024-02-19 13:15:05 -0500

    add chart to monitor YoY change

--- src/components/LoggedIn/Charts/Chart.js
+++ src/components/LoggedIn/Charts/Chart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import '../../LoggedIn/styles/Chart.css';
 
 const transformInvoiceData = (invoiceData) => {
@@ -30,39 +30,62 @@ const transformInvoiceData = (invoiceData) => {
 
   const transformedData = Object.keys(groupedData).map((formattedDate) => ({
     name: formattedDate,
-    Earnings: parseFloat(groupedData[formattedDate].toFixed(2)),
+    MonthlyEarnings: parseFloat(groupedData[formattedDate].toFixed(2)),
   }));
 
   return transformedData.sort((a, b) => new Date(a.name) - new Date(b.name));
 };
 
-const Chart = ({ invoiceData }) => {
+const transformInvoiceDataForYear = (invoiceData) => {
+  const paidInvoices = invoiceData.filter((invoice) => invoice.status === 'Paid');
+
+  const groupedData = paidInvoices.reduce((accumulator, invoice) => {
+    const date = new Date(invoice.invoiceDate);
+    const formattedDate = `${date.getFullYear()}`;
+
+    if (!accumulator[formattedDate]) {
+      accumulator[formattedDate] = 0;
+    }
+
+    accumulator[formattedDate] += parseFloat(invoice.totalAmount);
+    return accumulator;
+  }, {});
+
+  const transformedData = Object.keys(groupedData).map((formattedDate) => ({
+    name: formattedDate,
+    AnnualEarnings: parseFloat(groupedData[formattedDate].toFixed(2)),
+  }));
+
+  return transformedData.sort((a, b) => new Date(a.name) - new Date(b.name));
+};
+
+const MonthlyEarnings = ({ invoiceData }) => {
   const transformedData = transformInvoiceData(invoiceData);
 
   return (
     <div className="chart">
       <div className="chart-title">Monthly Revenue</div>
-      <ResponsiveContainer width="100%" aspect={2 / 1}>
+      <ResponsiveContainer width="100%" height={600}>
         <AreaChart
-          width={500}
-          height={400}
+          width={100}
           data={transformedData}
           margin={{
-            top: 10,
-            right: 30,
+            top: 20,
+            right: 50,
             left: 0,
             bottom: 0,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" className='chartGrid' />
-          <XAxis dataKey="name" stroke='gray' />
-          <YAxis />
-          <Tooltip label="Earnings" />
+          <CartesianGrid strokeDasharray="5 5" className='chartGrid' />
+          <XAxis dataKey="name" stroke='gray' tick={{ fill: '#555', fontSize: 12 }} />
+          <YAxis stroke='gray' tick={{ fill: '#555', fontSize: 12 }} />
+          <Tooltip label="Earnings" contentStyle={{ backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }} />
           <Area
             type="monotone"
-            dataKey="Earnings"
+            dataKey="MonthlyEarnings"
             stroke="#8884d8"
             fill="#8884d8"
+            legend={{ align: 'right', verticalAlign: 'top', height: 36 }}
           />
         </AreaChart>
       </ResponsiveContainer>
@@ -70,4 +93,38 @@ const Chart = ({ invoiceData }) => {
   );
 };
 
-export default Chart;
+const AnnualEarnings = ({ invoiceData }) => {
+  const transformedDataForYear = transformInvoiceDataForYear(invoiceData);
+
+  return (
+    <div className="chart">
+      <div className="chart-title">Annual Revenue</div>
+      <ResponsiveContainer width="100%" height={600}>
+        <AreaChart
+          width={800}
+          data={transformedDataForYear}
+          margin={{
+            top: 20,
+            right: 50,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="5 5" className='chartGrid' />
+          <XAxis dataKey="name" stroke='gray' tick={{ fill: '#555', fontSize: 12 }} />
+          <YAxis stroke='gray' tick={{ fill: '#555', fontSize: 12 }} />
+          <Tooltip label="Earnings" contentStyle={{ backgroundColor: '#fff', border: '1px solid #ccc', borderRadius: '5px', padding: '10px' }} />
+          <Area
+            type="monotone"
+            dataKey="AnnualEarnings"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+            legend={{ align: 'right', verticalAlign: 'top', height: 36 }}
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export { MonthlyEarnings, AnnualEarnings };
--- src/components/LoggedIn/Charts/Charts.js
+++ src/components/LoggedIn/Charts/Charts.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import Widget from './Widget';
 import Featured from './Featured';
-import Chart from './Chart';
+import { AnnualEarnings, MonthlyEarnings } from './Chart';
 import '../../LoggedIn/styles/Widget.css';
 
 const Charts = ({ customersData, inventoryData, invoiceData }) => {
   return (
     <>
       <div className="widgets">
-        <Widget type="customers" customersData={customersData}/>
-        <Widget type="inventory" inventoryData={inventoryData}/>
+        <Widget type="customers" customersData={customersData} />
+        <Widget type="inventory" inventoryData={inventoryData} />
         <Widget type="invoices" invoiceData={invoiceData} />
-        <Widget type="earnings" invoiceData={invoiceData}/>
+        <Widget type="earnings" invoiceData={invoiceData} />
       </div>
       <div className="charts">
-        <Featured invoiceData={ invoiceData }/>
-        <Chart invoiceData={invoiceData} />
+        <Featured invoiceData={invoiceData} />
+        <AnnualEarnings invoiceData={invoiceData} />
+        <MonthlyEarnings invoiceData={invoiceData} />
       </div>
     </>
   );
